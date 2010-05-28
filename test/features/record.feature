7f8c08ea397dcca72d0bb4e7a3a5b5152c46e638
Johan Euphrosine <proppy@aminche.com>
2010-05-28 23:41:35 +0200

    add test for record.feature

---
+++ test/features/record.feature
@@ -0,0 +1,33 @@
+Feature: Record API
+  Client should be able to edit Pincaster records throught its HTTP API
+  Scenario: create/get/delete
+    Given that Pincaster is started
+    Given that a layer named 'tlay' is created
+    When Client PUT /api/1.0/records/tlay/home.json 'description=Maison&who=Robert'
+    Then Pincaster returns:
+      """
+      {
+              "status": "stored"
+      }
+      """
+    When Client GET /api/1.0/records/tlay/home.json
+    Then Pincaster returns:
+      """
+      {
+	      "key": "home",
+	      "type": "hash",
+	      "properties": {
+		      "description": "Maison",
+		      "who": "Robert"
+	      }
+      }
+      """
+    When Client DELETE /api/1.0/records/tlay/home.json
+    Then Pincaster returns:
+      """
+      {
+              "status": "deleted"
+      }
+      """
+    When Client GET /api/1.0/records/tlay/home.json
+    Then Pincaster throws 404
--- test/features/step_definitions/pincaster.rb
+++ test/features/step_definitions/pincaster.rb
@@ -5,13 +5,17 @@ Before do
   @pid = fork { exec('../src/pincaster pincaster.conf') }
 end
 
+After do |scenario|
+  Process.kill("HUP", @pid)
+  sleep 1
+end
+
 Given /^that Pincaster is started$/ do
   sleep 1
 end
 
-After do |scenario|
-  Process.kill("HUP", @pid)
-  sleep 1
+Given /^that a layer named '(.*)' is created$/ do |layer|
+  RestClient.post 'localhost:4269/api/1.0/layers/'+layer+'.json', ''
 end
 
 When /^Client GET (.*)$/ do |path|
@@ -23,7 +27,7 @@ When /^Client GET (.*)$/ do |path|
   end
 end
 
-When /^Client POST (.*) (.*)$/ do |path, content|
+When /^Client POST (.*) '(.*)'$/ do |path, content|
   begin
     @result = JSON.parse(RestClient.post 'localhost:4269'+path, content)
     @result.delete('tid')
@@ -41,6 +45,15 @@ When /^Client DELETE (.*)$/ do |path|
   end
 end
 
+When /^Client PUT (.*) '(.*)'$/ do |path, content|
+  begin
+    @result = JSON.parse(RestClient.put 'localhost:4269'+path, content)
+    @result.delete('tid')
+  rescue Exception=>e
+    @result = e.class
+  end
+end
+
 Then /^Pincaster returns:$/ do |string|
   expected = JSON.parse(string)
   @result.should == expected
@@ -48,4 +61,8 @@ end
 
 Then /^Pincaster is dead$/ do
   @result.should == RestClient::ServerBrokeConnection
+end
+
+Then /^Pincaster throws 404$/ do
+  @result.should == RestClient::ResourceNotFound
 end
